ffa13c3d0d93fe6f137e8ce623fa3e6f6516c5dc
Lpz <1582630916@qq.com>
2025-07-17 17:08:57 +0800

    输入框组件开始编写

---
+++ BD/src/components/inputBox.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const inputType = ref
+</script>
+<template>
+  <div>
+    <input type="text" />
+  </div>
+</template>
+<style scoped lang="scss"></style>
