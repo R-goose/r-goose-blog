1950bc5d05da4e5ba031b0ce1f80519f3bc0f686
Lpz <1582630916@qq.com>
2025-07-22 16:33:10 +0800

    完善路由文件

--- BD/src/router/index.js
+++ BD/src/router/index.js
@@ -1,39 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import mainRouter from './mainRoutes'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name:'index',
-      component:()=>import('@/views/blogView/indexPage.vue'),
-      children:[
-        {
-          name:'blog',
-          path:'blog',
-          component:()=>import('@/views/blogView/index/blogPage.vue')
-        },
-        {
-          name:'social',
-          path:'social',
-          component:()=>import('@/views/blogView/index/socialPage.vue')
-        },
-        {
-          name:'personal',
-          path:'personal',
-          component:()=>import('@/views/blogView/index/personalPage.vue')
-        }
-      ]
-    },
-    {
-      name:'login',
-      path:'/login',
-      component:()=>import('@/views/blogView/login/loginPage.vue')
-
-    }
-
-  ],
+  routes: mainRouter
 })
 
 export default router
---
+++ BD/src/router/mainRoutes.js
@@ -0,0 +1,31 @@
+const mainRouter = [
+  {
+    path: '/',
+    name: 'index',
+    component: () => import('@/views/blogView/indexPage.vue'),
+    children: [
+      {
+        name: 'blog',
+        path: 'blog',
+        component: () => import('@/views/blogView/index/blogPage.vue'),
+      },
+      {
+        name: 'social',
+        path: 'social',
+        component: () => import('@/views/blogView/index/socialPage.vue'),
+      },
+      {
+        name: 'personal',
+        path: 'personal',
+        component: () => import('@/views/blogView/index/personalPage.vue'),
+      },
+    ],
+  },
+  {
+    name: 'login',
+    path: '/login',
+    component: () => import('@/views/blogView/login/loginPage.vue'),
+  },
+]
+
+export default mainRouter
