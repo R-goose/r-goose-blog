9f26bcf88b47f005ba548e68977efe2a0f9bdd77
kokomi <1582630916@qq.com>
2025-07-27 16:51:20 +0800

    修改主题切换按钮以及修复bug

--- BD/src/views/blogView/blog/mainPage.vue
+++ BD/src/views/blogView/blog/mainPage.vue
@@ -10,11 +10,20 @@ onMounted(() => {
   <div class="main text-center">
     <h1>Welcome to my blog</h1>
     <header></header>
+    <footer></footer>
   </div>
 </template>
 <style scoped lang="scss">
 .main {
-  height: 100vh;
+  height: 300vh;
+
+  footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 30vh;
+    background-color: #2c2c2c;
+  }
 }
 
 h1 {
