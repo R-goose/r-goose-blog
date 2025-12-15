de4e1b165f6d9ca1a2d2c3d9f717dd523bcc3323
linpengzhi <1582630916@qq.com>
2025-12-15 18:23:10 +0800

    博客页面开发

---
+++ BD/src/views/blogView/blog/myBlogComponents/pageTwo.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+<template>
+  <!-- 两页178vh -->
+  <div style="" class="myBlog2">
+    <!-- <h1>blog Page666</h1> -->
+
+    <div>文章内容区</div>
+
+  </div>
+</template>
+<style scoped lang="scss">
+.myBlog2 {
+  // background-color: #ac4c4c;
+  position: relative;
+  left: 3vw;
+  top: 1vh;
+  height: 89vh;
+  width: 98vw;
+  overflow: hidden;
+}
+</style>
